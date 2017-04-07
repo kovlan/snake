74dce3b0c479000cb4f261e0633668c8149b2ed1
Vladimir Kononov <el.kovlan@gmail.com>
2017-04-07 18:27:49 +0100

    first working version

---
+++ js/app.js
@@ -0,0 +1,211 @@
+//===================================
+// Field - current status of game field
+
+var Field = function(cols, rows, stone_percentage) {
+    console.assert(rows > 0 && cols > 0);
+    console.assert(rows < 1000 && cols < 1000);
+    console.assert(stone_percentage >- 0.0 && stone_percentage <= 20.0);
+
+    this.rows = rows;
+    this.cols = cols;
+    this.elems = new Array(rows);
+    for (var i = 0; i < rows; ++i) {
+        this.elems[i] = new Array(cols);
+        for (var j = 0; j < cols; ++j) {
+            this.elems[i][j] = 'none';
+        }
+    }
+
+    this.stones_num = Math.floor(rows * cols * stone_percentage);
+    for (var i = 0; i < this.stones_num; ++i) {
+        do {
+            var x = Math.floor((Math.random() * cols));
+            var y = Math.floor((Math.random() * rows));
+        } while (this.elems[y][x] != 'none');
+        this.elems[y][x] = 'stone';
+
+    }
+
+    this.food_elems = 0;
+    this.max_food_elems = 5;
+    this.foods = [];
+}
+
+Field.prototype.getWidth = function() {
+    return this.cols;
+}
+
+Field.prototype.getHeight = function() {
+    return this.rows;
+}
+
+Field.prototype.getValue = function(x, y) {
+    console.assert(x >= 0 && x < this.cols);
+    console.assert(y >= 0 && y < this.rows);
+    return this.elems[y][x];
+}
+
+Field.prototype.putElem = function(x, y, value) {
+    console.assert(x >= 0 && x < this.cols);
+    console.assert(y >= 0 && y < this.rows);
+
+    this.elems[y][x] = value;
+}
+
+Field.prototype.update = function(dt) {
+    if (this.food_elems < this.max_food_elems && Math.floor(Math.random() * 50) == 0) {
+        for (var i = 0; i < 20; ++i) {
+            var food_x = Math.floor((Math.random() * this.cols));
+            var food_y = Math.floor((Math.random() * this.rows));
+            if (this.elems[food_y][food_x] == 'none') {
+                this.elems[food_y][food_x] = 'food';
+                var ttl = Math.floor(Math.random() * 100) + 10;
+                this.foods.push({'x': food_x, 'y':food_y, 'ttl': ttl});
+                this.food_elems++;
+                break;
+            }
+        }
+    }
+
+    var new_foods = [];
+    for (var i in this.foods) {
+        this.foods[i].ttl -= 5 * dt;
+        if (this.foods[i].ttl > 0) {
+            new_foods.push(this.foods[i]);
+        } else {
+            this.putElem(this.foods[i].x, this.foods[i].y, 'none');
+            this.food_elems--;
+        }
+    }
+
+    this.foods = new_foods;
+}
+
+Field.prototype.eatFood = function(x, y) {
+    var result = 0;
+    var new_foods = [];
+    for (var i in this.foods) {
+        if (this.foods[i].x == x && this.foods.y == y) {
+            this.food_elems--;
+            result = Math.floor(this.foods[i].ttl / 10) + 1;
+        } else {
+            new_foods.push(this.foods[i]);
+        }
+    }
+
+    this.foods = new_foods;
+}
+
+
+
+
+
+//===================================
+// Snake - moving sequence of squares
+var Snake = function(field) {
+    this.curlen = 2;
+    this.feedlen = 2;
+    this.field = field;
+    this.cols = field.getWidth();
+    this.rows = field.getHeight();
+
+    do {
+        var x = Math.floor((Math.random() * (this.cols - 1)));
+        var y = Math.floor((Math.random() * this.rows));
+        var x1 = x + 1;
+    } while (field.getValue(x, y) != 'none' || field.getValue(x1, y) != 'none');
+
+    this.points = [ {'x': x, 'y': y}, {'x': x1, 'y': y} ];
+    this.head = 1;
+    this.tail = 0;
+
+    console.log([x, y, x1, y]);
+    field.putElem(x, y, 'snake');
+    field.putElem(x1, y, 'snake');
+
+    this.dir = { 'x' : 1, 'y' : 0};
+    this.dirname = 'right';
+    this.pos = 0.0;
+    this.speed = 5;
+    this.alive = true;
+}
+
+Snake.prototype.getHead = function() {
+    return this.points[this.head];
+}
+
+Snake.prototype.isAlive = function() {
+    return this.alive;
+}
+
+
+Snake.prototype.update = function(dt) {
+    if (!this.alive) {
+        return;
+    }
+
+    this.pos += this.speed * dt;
+    while (this.pos >= 1) {
+        var new_x = (this.points[this.head].x + this.dir.x) % this.cols;
+        var new_y = (this.points[this.head].y + this.dir.y) % this.rows;
+        if (new_x < 0) {
+            new_x = this.cols + new_x;
+        }
+        if (new_y < 0) {
+            new_y = this.rows + new_y;
+        }
+
+        var new_value = this.field.getValue(new_x, new_y);
+        if (new_value == 'food') {
+            var inc = this.field.eatFood(new_x, new_y);
+            this.feedlen++;
+        }
+
+        this.points.push({ 'x' : new_x, 'y' : new_y});
+        this.head++;
+
+        var old_tail = this.points[this.tail];
+        if (this.feedlen > this.curlen) {
+            this.curlen++;
+        } else {
+            this.field.putElem(old_tail.x, old_tail.y, 'none');
+            this.tail++;
+        }
+
+        if (new_value == 'stone' || new_value == 'snake') {
+            this.alive = false;
+            return;
+        }
+
+        this.field.putElem(new_x, new_y, 'snake');
+        this.pos -= 1;
+    }
+}
+
+
+Snake.prototype.turn = function(dirname) {
+    if (dirname == this.dirname) {
+        return;
+    }
+
+    if (dirname == 'left' && this.dirname != 'right') {
+        this.dir = {'x': -1, 'y': 0};
+        this.dirname = dirname;
+    }
+
+    if (dirname == 'right' && this.dirname != 'left') {
+        this.dir = {'x': 1, 'y': 0};
+        this.dirname = dirname;
+    }
+
+    if (dirname == 'up' && this.dirname != 'down') {
+        this.dir = {'x': 0, 'y': -1};
+        this.dirname = dirname;
+    }
+
+    if (dirname == 'down' && this.dirname != 'up') {
+        this.dir = {'x': 0, 'y': 1};
+        this.dirname = dirname;
+    }
+}
+
--- js/engine.js
+++ js/engine.js
@@ -7,9 +7,19 @@ var Engine = (function(global) {
         ctx = canvas.getContext('2d'),
         lastTime;
 
-    var canvas_width = 320;
-    var canvas_height = 550;
+    var border_size = 10;
+    var elem_width = 15;
+    var elem_height = 15;
+    var field_width = 40;
+    var field_height = 30;
+    var stone_percentage = 0.05;
+    
+    var canvas_width = elem_width * field_width + 2 * border_size;
+    var canvas_height = elem_height * field_height + 2 * border_size;
     var playing = true;
+    
+    var field = {};
+    var snake = {};
 
     canvas.width = canvas_width;
     canvas.height = canvas_height;
@@ -18,11 +28,15 @@ var Engine = (function(global) {
     function main() {
 
         var now = Date.now();
-            
+
         var dt = (now - lastTime) / 1000.0;
 
         update(dt);
         render();
+
+        if (!snake.isAlive()) {
+            playing = false;
+        }
 
         lastTime = now;
 
@@ -38,12 +52,85 @@ var Engine = (function(global) {
     }
 
     function update(dt) {
+        snake.update(dt);
+        field.update(dt);
+    }
+
+    function renderSquare(x, y, w, h, col) {
+        ctx.fillStyle = col;
+        ctx.fillRect(x, y, w, h);
+        ctx.fillStyle='black';
+    }
+
+    function renderCircle(x, y, w, h, col) {
+        ctx.fillStyle = col;
+        ctx.fillCircle(x, y, w, h);
+        ctx.fillStyle='black';
     }
 
     function render() {
+        // draw background
+        ctx.fillStyle='#CCCCCC';
+        ctx.fillRect(0, 0, canvas_width, canvas_height);
+        ctx.fillStyle='black';
+
+        // draw field border
+        var bottom_line_y = border_size + field_height * elem_height;
+        var right_line_x = border_size + field_width * elem_width;
+        ctx.beginPath();
+        ctx.moveTo(border_size, border_size);
+        ctx.lineTo(border_size, bottom_line_y);
+        ctx.lineTo(border_size + field_width * elem_width, bottom_line_y);
+        ctx.lineTo(right_line_x, border_size);
+        ctx.lineTo(border_size, border_size);
+        ctx.stroke();
+
+        // draw field elements
+        for (row = 0; row < field_height; row++) {
+            for (col = 0; col < field_width; col++) {
+                var elem_name = field.getValue(col, row);
+                switch (elem_name) {
+                case 'stone': 
+                    renderSquare(border_size + col * elem_width,
+                                 border_size + row * elem_height,
+                                 elem_width,
+                                 elem_height,
+                                 '#000000');
+                    break;
+                case 'snake':
+                    renderSquare(border_size + col * elem_width,
+                                 border_size + row * elem_height,
+                                 elem_width,
+                                 elem_height,
+                                 '#884400');
+                    break;
+                case 'food':
+                    renderSquare(border_size + col * elem_width,
+                                 border_size + row * elem_height,
+                                 elem_width,
+                                 elem_height,
+                                 '#00FF00');
+                    break;
+                }
+            }
+        }
+
+        // draw snake head
+        var head = snake.getHead();
+        var snake_color = '#FFFF00';
+        if (!snake.isAlive()) {
+            snake_color = '#FF0000';
+        }
+        renderSquare(border_size + head.x * elem_width,
+                     border_size + head.y * elem_height,
+                     elem_width,
+                     elem_height,
+                     snake_color);
     }
 
     function reset() {
+        field = new Field(field_width, field_height, stone_percentage);
+        snake = new Snake(field);
     }
 
     document.addEventListener('keydown', function(e) {
@@ -54,8 +141,13 @@ var Engine = (function(global) {
             40: 'down',
         };
 
+        // move
+        if (allowedKeys[e.keyCode]) {
+            snake.turn(allowedKeys[e.keyCode]);
+        }
+
     });
 
-    win.requestAnimationFrame(main);
+    win.requestAnimationFrame(init);
 
 })(this);
